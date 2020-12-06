d98ae4ebbf06434e4561f17373a2a6e634708b2a
hack-the-rat <55753991+hack-the-rat@users.noreply.github.com>
2020-12-06 14:50:54 -0500

    Create frontend.php

---
+++ release/frontend/php/frontend.php
@@ -0,0 +1,14 @@
+<?php
+	$array = json_decode($_POST['json_string']);
+	$curl = curl_init();
+	curl_setopt($curl, CURLOPT_POST, 1);
+	curl_setopt($curl, CURLOPT_URL, '../middleend/middleend.php');   
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $array);
+	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+	
+	$response = curl_exec($curl);
+	curl_close($curl);
+	
+	echo $response;
+?>
