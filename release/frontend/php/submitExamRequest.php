2671136dba6ce8aa258c56522294db33e718428a
hack-the-rat <55753991+hack-the-rat@users.noreply.github.com>
2020-12-06 14:51:17 -0500

    Add files via upload

--- release/frontend/php/submitExamRequest.php
+++ release/frontend/php/submitExamRequest.php
@@ -2,13 +2,13 @@
 	$array = json_decode($_POST['json_string']);
 	$curl = curl_init();
 	curl_setopt($curl, CURLOPT_POST, 1);
-	curl_setopt($curl, CURLOPT_URL, '../middleend/middleend.php');   
+	curl_setopt($curl, CURLOPT_URL, '../middleend/processExam.php');  
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($curl, CURLOPT_POSTFIELDS, $array);
 	curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-	
+
 	$response = curl_exec($curl);
 	curl_close($curl);
-	
+
 	echo $response;
 ?>
